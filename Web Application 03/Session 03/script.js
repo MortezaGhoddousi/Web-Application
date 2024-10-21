8a13a2ba676b01f04603a0df154bc729f2b53afc
Morteza <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-10-21 19:43:36 +0330

    Added session 03 to WA 03

---
+++ Web Application 03/Session 03/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $("li").on("click", function () {
+        $("li").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".carousel").carousel({
+        interval: 1000,
+        pause: false,
+        wrap: false,
+        keyboard: true,
+    });
+});
